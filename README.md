e89312b93f42aa0886b4fd13f5bc385b95694ab5
m1ga <migamiga@web.de>
2023-07-19 20:59:42 +0200

    update

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# MMKV module for Titanium SDK 
+# MMKV module for Titanium SDK
 
-> MMKV is an efficient, small, easy-to-use mobile key-value storage framework used in the WeChat application. 
+> MMKV is an efficient, small, easy-to-use mobile key-value storage framework used in the WeChat application.
 https://github.com/Tencent/MMKV
 
 ## Android
 
+#### Setup
+
+* create();
+
+#### Write values:
 * setBool(name, value)
 * setString(name, value)
 * setInt(name, value)
 
+#### Read values:
 * getBool(name, default value)
 * getString(name, default value)
 * getInt(name, default value)
+
+
+## Example
+
+see `examples/app.js` for a full example
--- example/app.js
+++ example/app.js
@@ -1,24 +1,20 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
-
-
-// open a single window
 const win = Ti.UI.createWindow();
 win.open();
 
-import ti_mmkv from 'ti.mmkv';
-ti_mmkv.create();
+import tiMMKV from 'ti.mmkv';
+tiMMKV.create();
 
-console.log(ti_mmkv.getBool("boolValue", false));
-console.log(ti_mmkv.getInt("intValue", 0));
-console.log(ti_mmkv.getString("stringValue", "default"));
+console.log("-----Read-----");
+console.log(tiMMKV.getBool("boolValue", false));
+console.log(tiMMKV.getInt("intValue", 0));
+console.log(tiMMKV.getString("stringValue", "default"));
 
-ti_mmkv.setBool("boolValue", true);
-ti_mmkv.setInt("intValue", 1337);
-ti_mmkv.setString("stringValue", "mmkv for Titanium SDK");
+console.log("-----Write-----");
+tiMMKV.setBool("boolValue", true);
+tiMMKV.setInt("intValue", 1337);
+tiMMKV.setString("stringValue", "mmkv for Titanium SDK");
 
-console.log(ti_mmkv.getBool("boolValue", false));
-console.log(ti_mmkv.getInt("intValue", 0));
-console.log(ti_mmkv.getString("stringValue", "default"));
+console.log("-----Read-----");
+console.log(tiMMKV.getBool("boolValue", false));
+console.log(tiMMKV.getInt("intValue", 0));
+console.log(tiMMKV.getString("stringValue", "default"));
